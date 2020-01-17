3d4898da5cded16bf60c3ece2430fd5f113cb20b
Alex Fenko <afenko@alfabank.ru>
2020-01-17 12:10:58 +0300

    feature/COMPONENTS-AMOUNT_regExp. Update tests

--- packages/utils/formatAmount.test.ts
+++ packages/utils/formatAmount.test.ts
@@ -1,10 +1,13 @@
 import { splitAmount } from './formatAmount';
 
-test('Split test for amount', () => {
+test('Split test for amount length < splitFrom (5 default)', () => {
     expect(splitAmount('100')).toBe('100');
-    expect(splitAmount('1000')).toBe('1 000');
-    expect(splitAmount('10000')).toBe('10 000');
-    expect(splitAmount('100000')).toBe('100 000');
-    expect(splitAmount('1000000')).toBe('1 000 000');
-    expect(splitAmount('10000000')).toBe('10 000 000');
+    expect(splitAmount('1000')).toBe('1000');
+})
+
+test('Split test for amount length >= splitFrom (5 default)', () => {
+    expect(splitAmount('10000')).toBe('10 000');
+    expect(splitAmount('100000')).toBe('100 000');
+    expect(splitAmount('1000000')).toBe('1 000 000');
+    expect(splitAmount('10000000')).toBe('10 000 000');
 })
